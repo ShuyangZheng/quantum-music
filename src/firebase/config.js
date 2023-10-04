60a0466304418d3d7f9f63f42bab052c8b41715d
Shuyang Zheng <shuyang014@gmail.com>
2023-10-04 15:51:45 -0400

    firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from "firebase/app";
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCt3CSzTaziDDg-Ff19HPusyXz9gVnpIqk",
+  authDomain: "muso-ninjas-cf138.firebaseapp.com",
+  projectId: "muso-ninjas-cf138",
+  storageBucket: "muso-ninjas-cf138.appspot.com",
+  messagingSenderId: "439882428070",
+  appId: "1:439882428070:web:9f7281fa4d5aef1ab18e67"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+// timestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectFirestore, projectAuth, timestamp };
