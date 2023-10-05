7493131c9b57b13fec6f9945f8134f6cd18887c2
Shuyang Zheng <shuyang014@gmail.com>
2023-10-04 21:24:00 -0400

    add navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="navbar">
+    <nav>
+      <img src="@/assets/ninja.png" alt="ninja">
+      <h1>
+        <router-link :to="{ name: 'home' }">Muso Ninjas</router-link>
+      </h1>
+      <div class="links">
+        <button>Logout</button>
+        <router-link class="btn" :to="{ name: 'signup'}">Signup</router-link>
+        <router-link class="btn" :to="{ name: 'login'}">Login</router-link>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+.navbar {
+  padding: 16px 10px;
+  margin-bottom: 60px;
+  background-color: white;
+}
+nav {
+  display: flex;
+  align-items: center;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+nav h1 {
+  margin-left: 20px;
+}
+nav .links {
+  margin-left: auto;
+}
+nav .links a, button {
+  margin-left: 16px;
+  font-size: 14px;
+}
+nav img {
+  max-height: 60px;
+}
+</style>
